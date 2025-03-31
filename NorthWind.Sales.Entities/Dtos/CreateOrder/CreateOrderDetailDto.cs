d65b98da2d38bb7d97b7bba28943e4a7b10a72f3
Clay Aldas <clayaldas@gmail.com>
2025-03-31 13:16:22 -0500

    Finalizada clases lunes 31-03-2025

---
+++ NorthWind.Sales.Entities/Dtos/CreateOrder/CreateOrderDetailDto.cs
@@ -0,0 +1,47 @@
+﻿namespace NorthWind.Sales.Entities.Dtos.CreateOrder;
+
+////  Constructores primarios (C# 12)
+public class CreateOrderDetailDto(int productId,
+    decimal unitPrice, short quantity)
+{
+    public int ProductId => productId;
+    public decimal UnitPrice => unitPrice;
+    public short Quantity => quantity;
+}
+
+
+//  C# 11-10-9-8
+//public class CreateOrderDetailDto
+//{
+//    public int ProductId { get; }
+//    public decimal UnitPrice {  get; }
+//    public short Quantity { get; }
+
+//    public CreateOrderDetailDto(int productId, decimal unitPrice, 
+//        short quantity)
+//    {
+//        ProductId = productId;
+//        UnitPrice = unitPrice;
+//        Quantity = quantity;
+//    }
+//}
+
+////  C# 7
+//public class CreateOrderDetailDto
+//{ 
+//    private readonly int _productId;
+//    private readonly decimal _unitPrince;
+//    private readonly short _quantity;
+
+//    public int ProductId { get { return _productId; } }
+//    public decimal UnitPrice { get { return _unitPrince; } }
+//    public short Quantity { get { return _quantity; } }
+
+//    public CreateOrderDetailDto(int productId, decimal unitPrice, 
+//        short quantity)
+//    {
+//        _productId = productId;
+//        _unitPrince = unitPrice;
+//        _quantity = quantity;
+//    }
+//}
---
+++ NorthWind.Sales.Entities/Dtos/CreateOrder/CreateOrderDto.cs
@@ -0,0 +1,13 @@
+﻿namespace NorthWind.Sales.Entities.Dtos.CreateOrder;
+
+public class CreateOrderDto (string customerID, string shipAddress, 
+    string shipCity, string shipCountry, string shipPostalCode, 
+    IEnumerable<CreateOrderDetailDto> orderDetails)
+{
+    public string CustomerID=> customerID;
+    public string ShipAddress => shipAddress;
+    public string ShipCity => shipCity;
+    public string ShipCountry => shipCountry;
+    public string ShipPostalCode => shipPostalCode;
+    public IEnumerable<CreateOrderDetailDto> OrderDetails => orderDetails;
+}
