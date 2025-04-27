55decfb8251f3a076691a0618fce67d06b4e4532
Clay Aldas <clayaldas@gmail.com>
2025-04-26 23:07:44 -0500

    Añadido DI DIP y IoC DIP por Zoom sabado 26-04-2025

---
+++ NorthWind.Sales.Backend.UseCases/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NorthWind.Sales.Backend.BusinessObjects.Interfaces.CreateOrder;
+using NorthWind.Sales.Backend.UseCases.CreateOrder;
+
+namespace NorthWind.Sales.Backend.UseCases;
+
+public static class DependencyContainer
+{
+  public static IServiceCollection AddUseCasesServices(this IServiceCollection services)
+  {
+    //  DI: Inyección de dependencias
+    //  DIP: inyeccion y enversion 
+    services.AddScoped<ICreateOrderInputPort, CreateOrderInteractor>();
+
+    // IoC Container: retorne el servicio con la inyeccion
+    return services;
+  }
+}
--- NorthWind.Sales.Backend.UseCases/obj/Debug/net8.0/NorthWind.Sales.Backend.UseCases.AssemblyInfo.cs
+++ NorthWind.Sales.Backend.UseCases/obj/Debug/net8.0/NorthWind.Sales.Backend.UseCases.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthWind.Sales.Backend.UseCases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4100653170455c05fd4f1635532f6d4bb718aac9")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthWind.Sales.Backend.UseCases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthWind.Sales.Backend.UseCases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
